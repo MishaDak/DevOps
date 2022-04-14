2352b2a237c76d7d974e767ef1c624960de967e5
MishaDak <47535566+MishaDak@users.noreply.github.com>
2022-04-14 10:13:53 +0700

    Update hello.go

--- hello.go
+++ hello.go
@@ -2,48 +2,36 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
+	"path"
 )
 
-func ReadTextFromFile(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Receive request path: %s\n", r.URL.Path)
-	if r.URL.Path[1:] == "" {
-		files, err := os.ReadDir("/tmp/files/")
-		if err != nil {
-			log.Fatal(err)
-		}
-		for _, file := range files {
-			fmt.Fprintf(w, "<h1> Hi, there!</h1>")
-			htmlstr := "<a href=" + file.Name() + ">" + file.Name() + "</a>\n"
-			fmt.Fprintf(w, htmlstr)
-		}
-	} else {
-		sff := "/tmp/files/" + r.URL.Path
-		file, err := os.Open(sff)
-
-		if err != nil {
-			fmt.Println(err)
-			fmt.Fprintf(w, "not file")
-		} else {
-			defer file.Close()
-
-			data := make([]byte, 64)
-
-			for {
-				n, err := file.Read(data)
-				if err == io.EOF {
-					break
-				}
-				fmt.Fprintf(w, string(data[:n]))
-			}
-		}
-	}
+func ErrorIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "/error")
 }
+
+func FileServerHandler(fs http.FileSystem) http.Handler {
+	fsh := http.FileServer(fs)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, err := fs.Open(path.Clean(r.URL.Path))
+		if os.IsNotExist(err) {
+			ErrorIndex(w, r)
+			return
+		}
+		if r.URL.Path[1:] == "" {
+			_, _ = fmt.Fprintf(w, "<h1>Hi, there!</h1>")
+			fsh.ServeHTTP(w, r)
+		} else {
+			fsh.ServeHTTP(w, r)
+		}
+	})
+}
+
 func main() {
+	http.Handle("/", FileServerHandler(http.Dir("./")))
+
 	fmt.Printf("Server started")
-	http.HandleFunc("/", ReadTextFromFile)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
